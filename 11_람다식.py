6946c570dc29cdd75610915daa8e1b02607330d2
kdh1101k <kdh1101k@naver.com>
2023-11-09 21:16:01 +0900

    ALTER 11_람다식.py 경과시간

--- 11_람다식.py
+++ 11_람다식.py
@@ -86,13 +86,14 @@ while game_on :
 endTime = time.time()
 
 
+print("경과 시간 없앰.")
+
 #startTime = time.time()
 #for i in range(99999) :
 #    print(i)
 #endTime = time.time()
 
 
-print(f'경과시간 : {endTime-startTime:.1f}s')
 
 
 
